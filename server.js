9b899f830cb4f713b431ffc8ce6c0af6b55c84c8
Adam Specker <specker@gmail.com>
2018-03-06 12:08:56 -0500

    heroku demo

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("Hello World!");
+
+app.get('*', function (req,res) {
+    res.sendFile(path.join(__dirname,'/index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
